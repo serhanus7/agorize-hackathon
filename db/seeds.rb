019b4456a7231fc1b3123605b22e839159bea41e
iserhan <i.serhan@eutech-ssii.com>
2018-11-12 16:25:44 +0000

    feat: seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Skills
+Skill.create id:1, name: "Football"
+Skill.create id:2, name: "Basketball"
+Skill.create id:3, name: "Foot"  , parent_id: 1
+Skill.create id:4, name: "Basket", parent_id: 2
+Skill.create id:5, name: "Soccer", parent_id: 1
+
+# Users
+User.create points: 100
+User.create points: 200
+User.create points: 100
+User.create points: 50
+User.create points: 10
+
+# Skills_users
+SkillsUser.create skill_id: 1, user_id:1
+SkillsUser.create skill_id: 1, user_id:2
+SkillsUser.create skill_id: 3, user_id:3
+SkillsUser.create skill_id: 2, user_id:4
+SkillsUser.create skill_id: 5, user_id:5
