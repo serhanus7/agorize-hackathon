2cab2058760c7e0f7588bcea2bc1c67ebae37d93
iserhan <i.serhan@eutech-ssii.com>
2018-11-14 16:06:10 +0000

    fix: update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,26 @@
 # Skills
-Skill.create id:1, name: "Football"
-Skill.create id:2, name: "Basketball"
-Skill.create id:3, name: "Foot"  , parent_id: 1
-Skill.create id:4, name: "Basket", parent_id: 2
-Skill.create id:5, name: "Soccer", parent_id: 1
+football = Skill.create name: "Football"
+basketball =Skill.create name: "Basketball"
+foot = Skill.create name: "Foot"  , parent_id: football.id
+basket = Skill.create name: "Basket", parent_id: basketball.id
+soccer = Skill.create name: "Soccer", parent_id: football.id
 
 # Users
-User.create points: 100
-User.create points: 200
-User.create points: 100
-User.create points: 50
-User.create points: 10
+user1 = User.create points: 100
+user2 = User.create points: 200
+user3 = User.create points: 100
+user4 = User.create points: 50
+user5 = User.create points: 10
+
+football.users << user1
+football.users << user2
+foot.users << user3
+basketball.users << user4
+soccer.users << user5
 
 # Skills_users
-SkillsUser.create skill_id: 1, user_id:1
-SkillsUser.create skill_id: 1, user_id:2
-SkillsUser.create skill_id: 3, user_id:3
-SkillsUser.create skill_id: 2, user_id:4
-SkillsUser.create skill_id: 5, user_id:5
+# SkillsUser.create skill_id: 1, user_id:1
+# SkillsUser.create skill_id: 1, user_id:2
+# SkillsUser.create skill_id: 3, user_id:3
+# SkillsUser.create skill_id: 2, user_id:4
+# SkillsUser.create skill_id: 5, user_id:5
